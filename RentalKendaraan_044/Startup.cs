38814bdb1baf377598c9aa12e83247eb7e95956e
Fakhrizar Rifqi Fuad <fakhrizar.rifqi.ft18@mail.umy.ac.id>
2020-11-28 22:25:10 +0700

    Membuat fitur upload file

--- RentalKendaraan_044/Startup.cs
+++ RentalKendaraan_044/Startup.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using RentalKendaraan_044.Models;
 
 namespace RentalKendaraan_044
 {
@@ -34,9 +38,9 @@ namespace RentalKendaraan_044
 
             services.AddDbContext<Models.RentKendaraanContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("Default")));
-
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSingleton<IFileProvider>(new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
